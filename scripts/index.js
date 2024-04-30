d12096cf35f348c6cd6e4a008097652839615fd0
Youssef Hafnawy <77534098+ymhaah@users.noreply.github.com>
2024-04-30 21:30:01 +0300

    done

---
+++ scripts/index.js
@@ -0,0 +1,77 @@
+// ? Add a message to the document title when the window loses focus
+window.addEventListener("blur", () => {
+    document.title = `We will miss you - ${document.title}`;
+});
+
+// ? Reset document title to the default title
+window.addEventListener("focus", () => {
+    document.title = `SmithHairston Travel`;
+});
+
+// ################# (header) #################
+const phoneNavList = document.querySelector(".header__main-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", toggleNavigation);
+window.addEventListener("scroll", () => {
+    const isNavOpen = navToggle.getAttribute("aria-expanded") === "true";
+
+    if (isNavOpen) {
+        closeNav();
+    }
+});
+
+// ? Toggles the navigation menu open/close states.
+function toggleNavigation() {
+    const isNavOpen = navToggle.getAttribute("aria-expanded") === "true";
+
+    if (isNavOpen) {
+        closeNav();
+    } else {
+        openNav();
+    }
+}
+
+// ? Opens the navigation menu.
+function openNav() {
+    // Update attributes
+    navToggle.setAttribute("aria-expanded", true);
+    phoneNavList.setAttribute("data-state", "opened");
+}
+
+// ? Closes the navigation menu.
+function closeNav() {
+    navToggle.setAttribute("aria-expanded", false);
+    phoneNavList.setAttribute("data-state", "closing");
+
+    // Listen for animation end to change data state to closed
+    phoneNavList.addEventListener("animationend", onAnimationEnd, {
+        once: true,
+    });
+}
+
+function onAnimationEnd() {
+    phoneNavList.setAttribute("data-state", "closed");
+}
+
+// ################# (form) #################
+
+const inputName = document.querySelector("input#input-name");
+const inputLocation = document.querySelector("input#input-location");
+const inputDestination = document.querySelector("input#input-destination");
+const inputDate = document.querySelector("input#input-date");
+const inputPrice = document.querySelector("input#input-price");
+const submitBtn = document.querySelector("#form-submit-btn");
+const submitMessage = document.querySelector("#submit-message");
+
+submitBtn.addEventListener("click", (e) => {
+    const name = inputName.value;
+    const location = inputLocation.value;
+    const destination = inputDestination.value;
+    const date = inputDate.value;
+    const price = inputPrice.value;
+
+    e.preventDefault();
+});
+
+console.log(`Made with 💙 by Youssef Hefnawy: https://twitter.com/hafanwy`);
