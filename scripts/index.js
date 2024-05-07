dbb2c74724ea92417c02b4c281dd30748e53dd74
Youssef Hafnawy <77534098+ymhaah@users.noreply.github.com>
2024-05-07 19:13:53 +0300

    Form

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-const URL = "";
+const URL =
+    "https://script.google.com/macros/s/AKfycbyIpGQppAA9pK1-gbhMJ5HbPTRcNOOjSBHbuVz5SIc-bnneKxhH9cU3my068enKuGP9NA/exec";
 
 // ? Add a message to the document title when the window loses focus
 window.addEventListener("blur", () => {
@@ -15,6 +16,8 @@ const phoneNavList = document.querySelector(".header__main-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", toggleNavigation);
+
+// ? Closes the navigation menu when scroll away.
 window.addEventListener("scroll", () => {
     const isNavOpen = navToggle.getAttribute("aria-expanded") === "true";
 
@@ -61,86 +64,196 @@ function onAnimationEnd() {
 // ################# (form) #################
 
 const form = document.querySelector("form");
-const submitMessage = document.getElementById("submit-message");
+let nameInput = document.getElementById("input-name");
+let emailInput = document.getElementById("input-email");
+let locationInput = document.getElementById("input-location");
+let destinationInput = document.getElementById("input-destination");
+let dateInput = document.getElementById("input-date");
+let priceRangeInput = document.getElementById("input-price");
+const submittedMessage = document.getElementById("submit-message");
 
-console.log(form);
+let travelDateBtn = document.getElementById("date-btn");
 
-submitMessage.addEventListener("click", (e) => {
-    e.preventDefault();
+travelDateBtn.addEventListener("click", () => {
+    dateInput.showPicker();
+});
+travelDateBtn.addEventListener("focus", () => {
+    dateInput.showPicker();
 });
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault(); // Prevent default form submission
+    event.preventDefault();
 
-    // let name = document.getElementById("input-name").value;
-    // let email = document.getElementById("input-email").value;
-    // let location = document.getElementById("input-location").value;
-    // let destination = document.getElementById("input-destination").value;
-    // let travelDate = document.getElementById("input-date-from").value;
-    // let priceRange = document.getElementById("input-price").value;
+    let name = nameInput.value;
+    let email = emailInput.value;
+    let location = locationInput.value;
+    let destination = destinationInput.value;
+    let date = dateInput.value;
+    let priceRange = priceRangeInput.value;
 
-    // function resetInput() {
-    //     name = "";
-    //     email = "";
-    //     location = "";
-    //     destination = "";
-    //     travelDate = "";
-    //     priceRange = "";
-    //     console.log("reset");
-    // }
+    // ? Function to reset input fields
+    function resetInput() {
+        nameInput.value = "";
+        emailInput.value = "";
+        locationInput.value = "";
+        destinationInput.value = "";
+        dateInput.value = "";
+        priceRangeInput.value = "";
+    }
+    // ? Function to update form state message
+    function formStateMessage(message, errorState) {
+        submittedMessage.textContent = message;
+        submittedMessage.setAttribute("date-form-error", errorState);
+    }
+    // ? Function to validate text input
+    function validateInput(input, fieldName) {
+        const trimmedValue = input.value.trim();
 
-    // // Form validation
-    // let isValid = true;
-    // submitMessage.textContent = "";
+        if (trimmedValue === "") {
+            formStateMessage(
+                `${fieldName} cannot be empty. Please enter some text.`,
+                "error"
+            );
+            input.select();
+            return false;
+        }
 
-    // if (
-    //     name.trim() === "" ||
-    //     name.trim().length === 0 ||
-    //     typeof name !== "string"
-    // ) {
-    //     submitMessage.textContent = "Please enter a valid name.";
-    //     isValid = false;
-    // }
-    // // if (!isValidEmail(email)) {
-    // //     submitMessage.textContent = "Please enter a valid email.";
-    // //     isValid = false;
-    // // }
+        if (/^\d+$/.test(trimmedValue)) {
+            formStateMessage(
+                `Invalid ${fieldName}. ${fieldName} cannot be just a number. Please enter valid text.`,
+                "error"
+            );
+            input.select();
+            return false;
+        }
 
-    // if (isValid) {
-    //     event.preventDefault();
-    //     // Submit form data to server
-    //     const formData = {
-    //         name,
-    //         email,
-    //         location,
-    //         destination,
-    //         travelDate,
-    //         priceRange,
-    //     };
+        if (/<[^>]*>?/gm.test(trimmedValue)) {
+            formStateMessage(
+                `Invalid ${fieldName}. ${fieldName} contains invalid characters. Please enter valid text.`,
+                "error"
+            );
+            input.select();
+            return false;
+        }
 
-    //     fetch(URL, {
-    //         method: "POST",
-    //         body: JSON.stringify(formData),
-    //         headers: { "Content-Type": "application/json" },
-    //     })
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             if (data.success) {
-    //                 resetInput();
-    //                 submitMessage.textContent = "Form submitted successfully!";
-    //             } else {
-    //                 submitMessage.textContent =
-    //                     "There was an error submitting the form.";
-    //                 console.error(data.error); // Log the error for debugging on the client-side
-    //             }
-    //         })
-    //         .catch((error) => {
-    //             resetInput();
-    //             console.error("Error submitting form:", error);
-    //             submitMessage.textContent =
-    //                 "There was an error submitting the form.";
-    //         });
-    // }
+        if (/[\x00-\x1F\x7F]/.test(trimmedValue)) {
+            formStateMessage(
+                `Invalid ${fieldName}. ${fieldName} contains control characters. Please enter valid text.`,
+                "error"
+            );
+            input.select();
+            return false;
+        }
+
+        if (/[^a-zA-Z0-9\s]/.test(trimmedValue)) {
+            formStateMessage(
+                `Invalid ${fieldName}. ${fieldName} contains disallowed special characters. Please enter valid text.`,
+                "error"
+            );
+            input.select();
+            return false;
+        }
+
+        return true;
+    }
+    // ? Function to validate email input
+    function validateEmail(input) {
+        const trimmedEmail = input.value.trim();
+
+        if (trimmedEmail === "") {
+            formStateMessage(
+                "Email address cannot be empty. Please enter a valid email.",
+                "error"
+            );
+            input.select();
+            return false;
+        }
+
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(trimmedEmail)) {
+            formStateMessage(
+                "Invalid email address format. Please enter a valid email.",
+                "error"
+            );
+            input.select();
+            return false;
+        }
+
+        if (/[\x00-\x1F\x7F]/.test(trimmedEmail)) {
+            formStateMessage(
+                "Email address contains control characters. Please enter a valid email.",
+                "error"
+            );
+            input.select();
+            return false;
+        }
+
+        if (/[^a-zA-Z0-9@._-]/.test(trimmedEmail)) {
+            formStateMessage(
+                "Email address contains disallowed special characters. Please enter a valid email.",
+                "error"
+            );
+            input.select();
+            return false;
+        }
+
+        return true;
+    }
+
+    // ? Submit form data to server
+    if (
+        validateInput(nameInput, "Name") &&
+        validateInput(locationInput, "Location") &&
+        validateInput(destinationInput, "Destination") &&
+        validateEmail(emailInput)
+    ) {
+        event.preventDefault();
+        let formData = new FormData(form);
+        var keyValuePairs = [];
+        for (var pair of formData.entries()) {
+            keyValuePairs.push(pair[0] + "=" + pair[1]);
+        }
+
+        var formDataString = keyValuePairs.join("&");
+
+        fetch(URL, {
+            redirect: "follow",
+            method: "POST",
+            body: formDataString,
+            headers: { "Content-Type": "text/plain;charset=utf-8" },
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.text();
+                } else {
+                    throw new Error("Failed to submit the form.");
+                }
+            })
+            .then((data) => {
+                if (data) {
+                    resetInput();
+                    formStateMessage("Form submit successfully.", "success");
+                    window.setTimeout(() => {
+                        formStateMessage(
+                            "Please enter your information to submit.",
+                            "waiting"
+                        );
+                    }, 10000);
+                } else {
+                    console.error(data);
+                    formStateMessage(
+                        "There was an error submitting the form. Please try again later.",
+                        "error"
+                    );
+                }
+            })
+            .catch((error) => {
+                console.error("Error submitting form:", error);
+                formStateMessage(
+                    "There was an error submitting the form. Please try agin later",
+                    "error"
+                );
+            });
+    }
 });
 
 console.log(`Made with 💙 by Youssef Hefnawy: https://twitter.com/hafanwy`);
